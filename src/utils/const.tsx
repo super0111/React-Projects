9f8293b7a1452ab223d7ef9b06822c74fd5d632d
SVEN <92024717+svendev1222@users.noreply.github.com>
2022-12-16 16:33:51 +0100

    first

--- src/utils/const.tsx
+++
@@ -1,3 +0,0 @@
-export const SECOND_IN_MILLI = 1000;
-export const TIMEOUT = 6000;
-export const ONE_DAY_IN_SECONDS = 24 * 3600;
--- src/utils/endpoint.tsx
+++
@@ -1,4 +0,0 @@
-export const endpointAccountToken = (apiAddress: string, walletAddress: string, tokenIdentifier: string) => {
-  const endpoint = `${apiAddress}/accounts/${walletAddress}/tokens/${tokenIdentifier}`;
-  return endpoint;
-};
